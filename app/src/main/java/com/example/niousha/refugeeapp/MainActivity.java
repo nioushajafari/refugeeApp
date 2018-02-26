d9dc70cbf33abbcaf027e4c92b1aba822630c548
niousha <niousha@nioushas-mbp.lan>
2018-02-25 22:04:07 -0500

    init

---
+++ app/src/main/java/com/example/niousha/refugeeapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.niousha.refugeeapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created by njafari on 2/25/18.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstances) {
+        super.onCreate(savedInstances);
+    }
+}
